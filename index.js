7ce30404e1d660cd9f72be8fdcdd7a19ffcf89df
Kamil Karczmarczyk <kkarczmarczyk@gmail.com>
2016-06-18 00:46:41 +0200

    API method for hardware sensor

--- index.js
+++ index.js
@@ -2,10 +2,10 @@ var express = require('express');
 var firebase = require('firebase');
 
 var app = express();
-var firebase_key = process.env.FIREBASE_KEY || require('./firebase.json').private_key;
-var config = {};
+var firebase_key = process.env.FIREBASE_KEY || require('./config.json').private_key;
+var token = process.env.TOKEN || require('./config.json').token;
+var live = {};
 
-console.log(firebase_key);
 firebase.initializeApp({
     serviceAccount: {
         projectId: "979611318308473915",
@@ -15,11 +15,18 @@ firebase.initializeApp({
     databaseURL: "https://project-979611318308473915.firebaseio.com/"
 });
 
-firebase.database().ref('/config').on('value', function(data) {
-    config = data.val();
+firebase.database().ref('/live').on('value', function(data) {
+    live = data.val();
 });
 
 app.set('port', (process.env.PORT || 5000));
+app.use('/api', function(req, res, next) {
+    if(req.query.token !== token) {
+        res.status(403).end('You must specify token');
+    } else {
+        next();
+    }
+});
 
 //app.use(express.static(__dirname + '/public'));
 
@@ -28,7 +35,19 @@ app.get('/', function(req, res) {
 });
 
 app.get('/api', function(req, res) {
-    res.json(config);
+    res.json(live);
+});
+
+app.get('/api/lap', function(req, res) {
+    res.end();
+    if(req.query.ms && live.race && live.user && live.status) {
+        if(live.status == "stop") {
+            firebase.database().ref('/live/status/').set("run");
+        } else {
+            firebase.database().ref('/race/' + live.race + '/' + live.user + '/').push(req.query.ms);
+        }
+
+    }
 });
 
 app.listen(app.get('port'), function() {
