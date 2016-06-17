5caa970ad4807b7a33a71746e519376e4d754959
Kamil Karczmarczyk <kkarczmarczyk@gmail.com>
2016-06-17 23:30:16 +0200

    keys

--- index.js
+++ index.js
@@ -2,15 +2,16 @@ var express = require('express');
 var firebase = require('firebase');
 
 var app = express();
-var firebase_key = process.env.FIREBASE_KEY || require('fs').readFileSync('./firebase.key');
+//var firebase_key = process.env.FIREBASE_KEY || require('fs').readFileSync('./firebase.key').toString();
+var firebase_key = process.env.FIREBASE_KEY || require('./firebase.json').private_key;
 var config = {};
 
-
+console.log(firebase_key);
 firebase.initializeApp({
     serviceAccount: {
         projectId: "979611318308473915",
         clientEmail: "backend@project-979611318308473915.iam.gserviceaccount.com",
-        privateKey: '-----BEGIN PRIVATE KEY-----\n' + firebase_key + '\n-----END PRIVATE KEY-----\n'
+        privateKey: firebase_key
     },
     databaseURL: "https://project-979611318308473915.firebaseio.com/"
 });
